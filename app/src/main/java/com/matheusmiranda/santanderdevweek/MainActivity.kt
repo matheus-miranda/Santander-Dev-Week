40ea1e7d4d4cf0a400fc274944c6af9aa20e0a78
matheus-miranda <matheus.sm86@gmail.com>
2021-05-28 18:41:43 -0300

    Create main layout

--- app/src/main/java/com/matheusmiranda/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/matheusmiranda/santanderdevweek/MainActivity.kt
@@ -2,10 +2,30 @@ package com.matheusmiranda.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item1 -> {
+                true
+            }
+            else -> true
+        }
     }
 }
